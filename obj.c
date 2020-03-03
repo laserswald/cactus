95d08a53980700209ba668528943f9b60dd3f873
Ben Davenport-Ray <ben.davenportray@azaleahealth.com>
2020-03-03 13:58:47 -0500

    wip

---
+++ obj.c
@@ -0,0 +1,15 @@
+#include "obj.h"
+
+const char *
+cact_obj_show_type(enum cact_obj_type t) {
+	switch (t) {
+	    case CACT_OBJ_PROCEDURE: return "procedure";
+	    case CACT_OBJ_ENVIRONMENT: return "environment";
+	    case CACT_OBJ_PAIR: return "pair";
+	    case CACT_OBJ_STRING: return "string";
+	    case CACT_OBJ_SYMBOL: return "symbol";
+	    case CACT_OBJ_PORT: return "port";
+	    case CACT_OBJ_ERROR: return "error";
+	}
+    return NULL;
+}
---
+++ vm.h
@@ -0,0 +1,13 @@
+#ifndef CACT_VM_H
+#define CACT_VM_H
+
+enum cact_vm_op {
+
+}
+
+struct cact_vm {
+    uint8_t ip;
+    uint8_t sp;
+}
+
+#endif /* CACT_VM_H */
