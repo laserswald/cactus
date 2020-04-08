4e2ee435ee439ae11f033f8b3daa13ccef837b02
Ben Davenport-Ray <ben.davenportray@azaleahealth.com>
2020-04-07 22:07:12 -0400

    Reformatted libs to kernel normal form

--- lib/env.c
+++ lib/env.c
@@ -17,36 +17,36 @@ TABLE_GENERATE(cact_env_entries, struct cact_symbol *, struct cact_val)
 struct cact_val
 cact_make_env(struct cactus *cact, struct cact_env *parent)
 {
-	struct cact_env *env = (struct cact_env *)cact_store_allocate(&cact->store, CACT_OBJ_ENVIRONMENT);
-	cact_env_init(env, parent);
-	return CACT_OBJ_VAL((struct cact_obj *) env);
+    struct cact_env *env = (struct cact_env *)cact_store_allocate(&cact->store, CACT_OBJ_ENVIRONMENT);
+    cact_env_init(env, parent);
+    return CACT_OBJ_VAL((struct cact_obj *) env);
 }
 
 void
 cact_env_init(struct cact_env *e, struct cact_env *parent)
 {
-	assert(e != parent);
-	e->parent = parent;
-	TABLE_INIT(&e->entries, ptrhash, cact_symbol_cmp);
+    assert(e != parent);
+    e->parent = parent;
+    TABLE_INIT(&e->entries, ptrhash, cact_symbol_cmp);
 }
 
 void
 cact_mark_env(struct cact_obj *o)
 {
     struct cact_env *e;
-	struct TABLE_ENTRY(cact_env_entries) *bucket;
+    struct TABLE_ENTRY(cact_env_entries) *bucket;
 
-	e = (struct cact_env *) o; 
+    e = (struct cact_env *) o;
 
-	TABLE_FOREACH_BUCKET(, bucket, &e->entries) {
-		if (bucket->state == TABLE_ENTRY_FILLED) {
-			cact_mark_val(bucket->val);
-		}
-	}
+    TABLE_FOREACH_BUCKET(, bucket, &e->entries) {
+        if (bucket->state == TABLE_ENTRY_FILLED) {
+            cact_mark_val(bucket->val);
+        }
+    }
 
-	if (e->parent) {
-		cact_mark_env((struct cact_obj *) e->parent);
-	}
+    if (e->parent) {
+        cact_mark_env((struct cact_obj *) e->parent);
+    }
 }
 
 void
@@ -54,7 +54,7 @@ cact_destroy_env(struct cact_obj *o)
 {
     struct cact_env *e;
 
-	e = (struct cact_env *) o; 
+    e = (struct cact_env *) o;
 
     TABLE_CLEAR(&e->entries);
 }
@@ -65,20 +65,20 @@ cact_destroy_env(struct cact_obj *o)
 struct cact_val
 cact_env_lookup(struct cactus *cact, struct cact_env *e, struct cact_symbol *key)
 {
-	assert(e);
-	assert(key);
+    assert(e);
+    assert(key);
 
-	struct cact_val *found = TABLE_FIND(cact_env_entries, &e->entries, key);
+    struct cact_val *found = TABLE_FIND(cact_env_entries, &e->entries, key);
 
-	if (! found) {
-		if (e->parent) {
-			return cact_env_lookup(cact, e->parent, key);
-		} else {
-			return cact_make_error(cact, "env lookup: no variable with key", CACT_SYM_VAL(key));
-		}
-	}
+    if (! found) {
+        if (e->parent) {
+            return cact_env_lookup(cact, e->parent, key);
+        } else {
+            return cact_make_error(cact, "env lookup: no variable with key", CACT_SYM_VAL(key));
+        }
+    }
 
-	return *found;
+    return *found;
 }
 
 /*
@@ -89,16 +89,16 @@ struct cact_val
 cact_env_define(struct cactus *cact, struct cact_env *e,
                 struct cact_symbol *key, struct cact_val val)
 {
-	assert(e);
-	assert(key);
+    assert(e);
+    assert(key);
 
-	if (TABLE_HAS(cact_env_entries, &e->entries, key)) {
-		return cact_make_error(cact, "cannot define variable, already exists", CACT_SYM_VAL(key));
-	}
+    if (TABLE_HAS(cact_env_entries, &e->entries, key)) {
+        return cact_make_error(cact, "cannot define variable, already exists", CACT_SYM_VAL(key));
+    }
 
-	TABLE_ENTER(cact_env_entries, &e->entries, key, val);
+    TABLE_ENTER(cact_env_entries, &e->entries, key, val);
 
-	return CACT_UNDEF_VAL;
+    return CACT_UNDEF_VAL;
 }
 
 /* Assign the key to the value, ensuring the key already exists. */
@@ -106,37 +106,37 @@ struct cact_val
 cact_env_set(struct cactus *cact, struct cact_env *e,
              struct cact_symbol *key, struct cact_val val)
 {
-	assert(e);
-	assert(key);
+    assert(e);
+    assert(key);
 
-	if (! TABLE_HAS(cact_env_entries, &e->entries, key)) {
-		return cact_make_error(cact, "cannot set nonexistent variable", CACT_SYM_VAL(key));
-	}
+    if (! TABLE_HAS(cact_env_entries, &e->entries, key)) {
+        return cact_make_error(cact, "cannot set nonexistent variable", CACT_SYM_VAL(key));
+    }
 
-	TABLE_ENTER(cact_env_entries, &e->entries, key, val);
+    TABLE_ENTER(cact_env_entries, &e->entries, key, val);
 
-	return CACT_UNDEF_VAL;
+    return CACT_UNDEF_VAL;
 }
 
 void
 print_env(struct cact_env *e)
 {
-	if (!e)
-		return;
+    if (!e)
+        return;
 
-	struct TABLE_ENTRY(cact_env_entries) *bucket;
-	TABLE_FOREACH_BUCKET(, bucket, &e->entries) {
-		if (bucket->state == TABLE_ENTRY_FILLED) {
-			printf("%s : ", bucket->key->sym);
-			cact_display(bucket->val);
-			printf("\n");
-		}
-	}
+    struct TABLE_ENTRY(cact_env_entries) *bucket;
+    TABLE_FOREACH_BUCKET(, bucket, &e->entries) {
+        if (bucket->state == TABLE_ENTRY_FILLED) {
+            printf("%s : ", bucket->key->sym);
+            cact_display(bucket->val);
+            printf("\n");
+        }
+    }
 
-	if (e->parent) {
-		printf("parent:\n");
-		print_env(e->parent);
-	}
+    if (e->parent) {
+        printf("parent:\n");
+        print_env(e->parent);
+    }
 
 }
 
--- lib/str.c
+++ lib/str.c
@@ -7,27 +7,27 @@
 struct cact_val
 cact_make_string(struct cactus *cact, const char *str)
 {
-	struct cact_string *s;
+    struct cact_string *s;
 
-	s = (struct cact_string *)cact_store_allocate(&cact->store, CACT_OBJ_STRING);
-	s->str = xstrdup(str);
+    s = (struct cact_string *)cact_store_allocate(&cact->store, CACT_OBJ_STRING);
+    s->str = xstrdup(str);
 
-	return CACT_OBJ_VAL(s);
+    return CACT_OBJ_VAL(s);
 }
 
-void 
+void
 cact_mark_string(struct cact_obj *ignored)
 {
-	return;
+    return;
 }
 
-void 
+void
 cact_destroy_string(struct cact_obj *o)
 {
-	struct cact_string *s;
+    struct cact_string *s;
 
-	s = (struct cact_string *) o;
+    s = (struct cact_string *) o;
 
-	xfree(s->str);
+    xfree(s->str);
 }
 
