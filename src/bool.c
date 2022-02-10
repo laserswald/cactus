9c88281f01d0007986772bb5c55aa74cfde243df
Ben Davenport-Ray <ben.davenportray@gmail.com>
2022-02-10 13:39:39 -0500

    Document bool.c

--- src/bool.c
+++ src/bool.c
@@ -1,11 +1,20 @@
 #include "bool.h"
 
+/*
+ * Return true if this value is true according to an 
+ * if expression. 
+ * 
+ * That is, not #f.
+ */
 bool
 cact_is_truthy(const struct cact_val x)
 {
     return ! cact_is_bool(x) || cact_to_bool(x, "truthy?"); // adsfkja;dslkjf;lkdsaj
 }
 
+/*
+ * Return #t if the value is #f, and #f if the value is otherwise.
+ */
 struct cact_val
 cact_bool_not(const struct cact_val x)
 {
